2875f4329753c4c0544d6801e694b314f3f257b7
cheuyin <yinstanleycheung@gmail.com>
2024-10-05 15:01:06 -0700

    Change localhost to 127.0.01

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -86,7 +86,7 @@ function App() {
   const handleSubmit = async () => {
     try {
       const response = await axios.post(
-        'http://localhost:5000/generate-trip',
+        'http://127.0.0.1:5000/generate-trip',
         formData
       );
       console.log(response);
